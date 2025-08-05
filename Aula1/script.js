6a36c6d4e2fe6ca8dc6e1d0b7906dd6bfd61fca2
AlexLPinheiro <alexlp2k6@gmail.com>
2025-08-05 17:00:03 -0300

    primeira aula

---
+++ Aula1/script.js
@@ -0,0 +1,40 @@
+/*let contador = 1;
+const texto = document.getElementById("Texto")
+let textoFinal = ""
+for(contador = 1;contador<=5;contador++){
+    console.log(contador)
+}
+
+class Person {
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    falarNome(){
+        return "Ola meu nome é "+this.name+" e tenho "+this.age+" anos"
+    }
+}
+const pessoa1 = new Person("Alex", 18)
+textoFinal = document.createTextNode(pessoa1.falarNome())
+texto.appendChild(textoFinal)
+console.log(textoFinal)
+*/
+
+let numero = 3;
+console.log(numero);
+numero =5;
+console.log(numero);
+/*const comidaFavorita = prompt("comida favorita?")
+alert(comidaFavorita)
+*/
+let b = confirm("Vc me ama?")
+if (b == true){
+    alert("eu tambem te amo")
+}
+else{
+    alert("Vai toma no cu piranha")
+}
+
+
+
+
